9655bf27e47d379c909208121d212a057122b557
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-09 20:52:05 +0530

    ServerJS: server.js cleanup & Files Linked.
    1. Server.js is cleaned up and files and relevant routers are now linked
       up.

--- server.js
+++ server.js
@@ -3,33 +3,24 @@ require('dotenv').config();
 
 //imports
 const express = require('express');
-const { spawn, execFile } = require('child_process');
+
+//file imports
+const bloat_router = require('./routers/bloat.js');
+const language_router = require('./routers/language_router.js');
 
 //constants
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    var largeDataSet = [];
-    // spawn new child process to call the python script
-    const python = spawn('python', ['test.py']);
-    python.stdout.setEncoding('utf8');
-    // collect data from script
-    python.stdout.on('data', function (data) {
-        largeDataSet.push(data);
-        console.log(data.toString(), end='');
-    });
-    // in close event we are sure that stream is from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        res.send(largeDataSet.join(""))
-    });
+//app uses
+app.use(bloat_router);
+app.use('/language', language_router);
 
-});
-app.get('/hmm', (req, res) => {
-    res.send()
+//404 on all pages not found
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404</h1>');
 })
+
 app.listen(port, () => {
     console.log(`Example app listening on port! ${port}`);
 });
