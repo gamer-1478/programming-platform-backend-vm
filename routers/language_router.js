07b2f6a3bad87fc8815c5866ac6524620fda21c7
Aayush Garg <aayushgarg2006@gmail.com>
2022-02-18 23:12:51 +0530

    Routers: Add Python2 Support

--- routers/language_router.js
+++ routers/language_router.js
@@ -84,6 +84,80 @@ lang_router.post('/python3', verifyServerIdentity, async (req, res) => {
     }, 15000);
 });
 
+lang_router.post('/python2', verifyServerIdentity, async (req, res) => {
+    var code_to_execute = req.body.code;
+    var input_exec = req.body.input_exec;
+    var id = uuidv4();
+    var outputDataSet = [];
+    var errDataSet = [];
+    var exitData = { code: 99, signal: 0 };
+    var running = true;
+
+    //check for folder existence, if it doesn't exist create it
+    if (!fs.existsSync('./code_exec/python/')) {
+        fs.mkdirSync('./code_exec/python/', { recursive: true });
+    }
+
+    //create a file with the id
+    fs.writeFileSync(`./code_exec/python/${id}.py`, code_to_execute, { flag: 'w' });
+
+    // spawn a child process to run the python script
+    const python = spawn('python2', [`./code_exec/python2/${id}.py`]);
+
+    // collect data from script
+    python.stdout.on('data', function (data) {
+        outputDataSet.push(data.toString());
+        //console.log(data.toString(), end = '');
+    });
+
+    // send input to spawned process
+    python.stdin.write(input_exec.join('\n') + '\n');
+    python.stdin.end();
+
+    // store errors raised during execution
+    python.stderr.on('data', function (data) {
+        //console.log('stdout: ' + data);
+        errDataSet.push(data.toString());
+    });
+
+    // store spawn error
+    python.on('error', (err) => {
+        errDataSet.push(err.toString());
+        //console.log(err.toString(), end = '');
+    });
+
+    // store exit data
+    python.on('exit', (code, signal) => {
+        // console.log("exit", code, signal);
+        exitData.code = code;
+        exitData.signal = signal;
+    });
+
+    // send data to client
+    python.on('close', (code) => {
+        newOutputDataSet = serialiseOutput(outputDataSet);
+
+        //console.log(`child process close all stdio with code ${code}`);
+        res.send({
+            data: newOutputDataSet,
+            code: code,
+            err: errDataSet,
+            exit: exitData
+        });
+        running = false;
+        fs.unlinkSync(`./code_exec/python/${id}.py`);
+    });
+
+    /* if the program has not executed within a given time frame lets say x seconds, its terminated.
+       NOTE: We Depend On Close Event Broadcast To Send Response To Client. No Response Handling is needed here.*/
+    setTimeout(() => {
+        if (running) {
+            python.stdin.pause();
+            python.kill();
+        }
+    }, 15000);
+});
+
 lang_router.post('/gcc', verifyServerIdentity, async (req, res) => {
     var outputDataSet = [];
     var code_to_execute = req.body.code;
